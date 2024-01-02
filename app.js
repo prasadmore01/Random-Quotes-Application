987d5bcb5ae59ee38827c1d83dd42bcad8d951cf
Prasad More <prasadmoremain@gmail.com>
2024-01-02 18:22:05 +0530

    Displayed Quotes and author and added functionality

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-fetch("https://type.fit/api/quotes").then((data)=>{
-    return data.json();
-}).then((data)=>{
-    console.log(data)
-})
 
 
+let getQuote = ()=>{
+    fetch("https://type.fit/api/quotes").then((data)=>{
+        return data.json();
+    }).then((data)=>{
+        let qut = document.getElementById('quote')
+        let aut = document.getElementById('author')
+    
+        let random = Math.floor(Math.random()*data.length)
+    
+        qut.innerText = data[random].text
+        aut.innerText = data[random].author
+    
+    })
+    
+
+}
+
+getQuote()
